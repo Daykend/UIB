902912423d3b46f60ac21fd863857c9b829bb4fd
Daykend Lira <daykendvlira@gmail.com>
2020-03-13 12:58:10 -0300

    Implantação do Menu UIB

--- src/application/UIBMain.java
+++ src/application/UIBMain.java
@@ -14,26 +14,31 @@ public class UIBMain {
 		
 		System.out.print("Quantos cadastros serão realizados: ");
 		final int TOTAL_DE_CADASTROS = sc.nextInt();
-		System.out.println();
-		
-		System.out.println("---------------------------------------");
-		
 		Cliente clientes[] = new Cliente[TOTAL_DE_CADASTROS];
 		Conta contas[] = new Conta[TOTAL_DE_CADASTROS];
+		
+		
+		int opcao = 99;
+		do {
+			System.out.println("Bem Vindo ao Unit Internet Bank!");
+			System.out.println("--------------------------------");
+			System.out.println();
+			
+			
+		} while(opcao != 0);
+		
+		
 		
 		for (int i=0; i < TOTAL_DE_CADASTROS; i++) {
 			clientes[i] = new Cliente();
 			contas[i] = new Conta();
-			System.out.print("Digite o nome do Cliente: ");
-			clientes[i].setName(sc.nextLine());
-			System.out.println();
-			System.out.print("Digite o CPF: ");
+			System.out.printf("Digite o nome do %d° Cliente:%n", i);
+			clientes[i].setName(sc.next());
+			System.out.println("Digite o CPF: ");
 			clientes[i].setCpf(sc.next());
-			System.out.println();
 			contas[i].setNumber(1001 + i);
-			System.out.print("Digite o deposito inical para conta de " + clientes[i].getName() + ": ");
+			System.out.println("Digite o deposito inical para conta de " + clientes[i].getName() + ": ");
 			contas[i].creditar(sc.nextDouble());
-			System.out.println();
 			contas[i].setCliente(clientes[i]);
 			System.out.println("---------------------------------------");
 			contas[i].setCliente(clientes[i]);
